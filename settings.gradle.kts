8ddc03064f3659405e01e21e452df395ff163ce8
xxaleebxx <112494911+xxaleebxx@users.noreply.github.com>
2023-11-21 11:19:37 -0500

    initialize android studio

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CIS357 Semester Project"
+include(":app")
+ 
