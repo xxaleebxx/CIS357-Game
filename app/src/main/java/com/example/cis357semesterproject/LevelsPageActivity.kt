dc3e3e4f576dbcf8d110bd11791262026df9d7a9
xxaleebxx <112494911+xxaleebxx@users.noreply.github.com>
2023-11-27 17:13:21 -0500

    add levels page

---
+++ app/src/main/java/com/example/cis357semesterproject/LevelsPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cis357semesterproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LevelsPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_levels_page)
+    }
+}
