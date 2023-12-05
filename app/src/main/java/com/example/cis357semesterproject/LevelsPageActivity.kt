65d0abc4669346cd518657f5d44b934775f2e723
xxaleebxx <112494911+xxaleebxx@users.noreply.github.com>
2023-12-04 20:08:39 -0500

    Add logic to main menu and levels menu buttons

--- app/src/main/java/com/example/cis357semesterproject/LevelsPageActivity.kt
+++ app/src/main/java/com/example/cis357semesterproject/LevelsPageActivity.kt
@@ -1,11 +1,27 @@
 package com.example.cis357semesterproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class LevelsPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_levels_page)
+
+        val   mainMenuButton  = findViewById<Button>(R.id.mainmenu_button)
+        mainMenuButton.setOnClickListener {
+            val mainMenuIntent = Intent(this@LevelsPageActivity, MainActivity::class.java)
+            startActivity(mainMenuIntent)
+        }
+
+
+        val   levelOneButton  = findViewById<ImageButton>(R.id.levelOne_button)
+        levelOneButton.setOnClickListener {
+            val levelOneIntent = Intent(this@LevelsPageActivity, LevelActivity::class.java)
+            startActivity(levelOneIntent)
+        }
     }
 }
--- app/src/main/java/com/example/cis357semesterproject/MainActivity.kt
+++ app/src/main/java/com/example/cis357semesterproject/MainActivity.kt
@@ -1,27 +1,23 @@
 package com.example.cis357semesterproject
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import com.example.cis357semesterproject.databinding.ActivityMainBinding
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        val   mainPlayButton  = findViewById<Button>(R.id.MainPlay_button)
+        mainPlayButton.setOnClickListener {
+            val mainPlayIntent = Intent(this@MainActivity, LevelsPageActivity::class.java)
+            startActivity(mainPlayIntent)
+        }
 
     }
 
