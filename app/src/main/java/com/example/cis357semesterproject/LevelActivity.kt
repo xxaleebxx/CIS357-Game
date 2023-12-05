75e70924b62bcc5ba3eb764ca982a5545a9934dc
xxaleebxx <112494911+xxaleebxx@users.noreply.github.com>
2023-12-04 21:01:59 -0500

    Remove old fragments and add logic to LevelActivity buttons

--- app/src/main/java/com/example/cis357semesterproject/LevelActivity.kt
+++ app/src/main/java/com/example/cis357semesterproject/LevelActivity.kt
@@ -1,11 +1,47 @@
 package com.example.cis357semesterproject
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.media.Rating
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class LevelActivity : AppCompatActivity() {
+    private var levelOneTime: String? = null //this time is the final time
+    private var levelOneRunningTime: String? = null //this is the current time
+    private var levelOneRating: Rating? = null
+    private var levelOneStarted: Boolean = false
+
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level)
+
+
+        val   playButton  = findViewById<Button>(R.id.play_button)
+
+        playButton.setOnClickListener {
+            val playIntent = Intent(this@LevelActivity, TiltMazeActivity::class.java)
+            levelOneStarted = true
+            startActivity(playIntent)
+        }
+
+        val   levelsButton  = findViewById<Button>(R.id.levels_button)
+
+        levelsButton.setOnClickListener {
+            val levelsIntent = Intent(this@LevelActivity, LevelsPageActivity::class.java)
+            startActivity(levelsIntent)
+        }
+
+
+
+        if (!levelOneStarted && levelOneTime.isNullOrEmpty()) {
+                playButton.text = "Play"
+        } else if (levelOneStarted && levelOneTime.isNullOrEmpty()){
+            playButton.text = "Resume"
+        } else { playButton.text = "Restart"}
     }
 }
--- app/src/main/java/com/example/cis357semesterproject/LevelsPageActivity.kt
+++ app/src/main/java/com/example/cis357semesterproject/LevelsPageActivity.kt
@@ -1,27 +1,45 @@
 package com.example.cis357semesterproject
 
 import android.content.Intent
+import android.media.Rating
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
 
 class LevelsPageActivity : AppCompatActivity() {
+    // variables to carry information from the LevelsActivity
+    private var levelOneTime: String? = null
+    private var levelOneRating: Rating? = null
+    private var levelOneStarted: Boolean = false
+    var navCtrl : NavController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_levels_page)
 
         val   mainMenuButton  = findViewById<Button>(R.id.mainmenu_button)
+        val   levelOneButton  = findViewById<ImageButton>(R.id.levelOne_button)
+
+
         mainMenuButton.setOnClickListener {
             val mainMenuIntent = Intent(this@LevelsPageActivity, MainActivity::class.java)
             startActivity(mainMenuIntent)
         }
-
-
-        val   levelOneButton  = findViewById<ImageButton>(R.id.levelOne_button)
         levelOneButton.setOnClickListener {
             val levelOneIntent = Intent(this@LevelsPageActivity, LevelActivity::class.java)
             startActivity(levelOneIntent)
         }
+
     }
+
+
 }
